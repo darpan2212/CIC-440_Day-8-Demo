19debf14ff94316d9d2d5ba2aac5813a09e5e375
Darpan Madlani <Darpan@proclink.com>
2022-02-03 10:17:11 +0530

    dictionaryExample and git push script

--- gitPush.sh
+++ gitPush.sh
@@ -2,7 +2,7 @@ echo "---------Git Add------------------";
 git add -A;
 
 echo "-----------Git commit-------------";
-git commit -m '$1';
+git commit -m "$1";
 
 echo "------------Git push-------------";
 git push origin $2;
